e278656820d9392dd4234e3db9835302cc62c5a3
alexe <alexey-kor2@yandex.ru>
2026-08-29 23:57:54 +0300

    working with Alerts

---
+++ test_third_1_alert.py
@@ -0,0 +1,23 @@
+from time import sleep
+
+from playwright.sync_api import Page, Route, Dialog
+import re
+'''
+    pytest --headed -v -s test_third_1_alert.py::test_alert
+'''
+def test_alert(page: Page):
+    page.goto("https://demoblaze.com")
+
+    def accept_alert(alert: Dialog):
+        print(alert.message)
+        alert.accept()
+
+    page.on('dialog', accept_alert)
+    page.get_by_role('link', name='Samsung galaxy s6').click()
+    page.get_by_role('link', name='Add to cart').click()
+    page.wait_for_event('dialog')
+    page.locator('#cartur').click()
+    sleep(5)
+
+
+
